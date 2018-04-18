e1da6c3d4c5fd6c8ff17c5461dce8eb4e30ce2aa
Sristi, Vns Murthy <vns.murthy.sristi@intel.com>
2018-04-18 12:09:35 +0530

    Removed unused graphfiles
    Signed-off-by: Sristi, Vns Murthy <vns.murthy.sristi@intel.com>

--- Intel_movidius_nn_hal/ncs_lib_operations/Android.mk
+++ Intel_movidius_nn_hal/ncs_lib_operations/Android.mk
@@ -13,18 +13,3 @@ LOCAL_MODULE := libncs_nn_operation
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := graph_sigm
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/vendor/firmware/mvnc
-LOCAL_SRC_FILES := graphfiles/graph_sigm
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := graph_tanh
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/vendor/firmware/mvnc
-LOCAL_SRC_FILES := graphfiles/graph_tanh
-include $(BUILD_PREBUILT)
