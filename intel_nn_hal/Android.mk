275fcb693792d05729cdc34ae385a53568299e03
Lakshmishree C <Lakshmishree C lakshmishree.c@intel.com>
2020-03-11 14:41:04 +0530

    Fix for NNHAL klocwork issues and cflags added for SDL
    Tracked-On: OAM-89588
    Signed-off-by: Lakshmishree C <Lakshmishree C lakshmishree.c@intel.com>

--- intel_nn_hal/Android.mk
+++ intel_nn_hal/Android.mk
@@ -49,7 +49,13 @@ LOCAL_CFLAGS += \
 	-Wno-error \
 	-D_FORTIFY_SOURCE=2 \
 	-fvisibility=default \
-	-fexceptions
+	-fexceptions \
+	-fwrapv \
+    -fstack-protector-strong \
+    -Wno-conversion-null \
+    -Wnull-dereference \
+    -Warray-bounds \
+	-O0
 
 LOCAL_CFLAGS += \
 	-DIMPLEMENT_INFERENCE_ENGINE_API \
