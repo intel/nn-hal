b3e2c2ff98f830e09539f2c1217aa93c05f4eaf7
Raviraj Sitaram P <raviraj.p.sitaram@intel.com>
2021-03-23 13:56:22 +0530

    Fixes which allows the graph to run in NNHAL

--- intel_nn_hal/Dequantize.h
+++ intel_nn_hal/Dequantize.h
@@ -27,13 +27,10 @@ class DequantizeOp : public BaseOp {
         bool setInputData(void* dataPtr, uint32_t len) {
             inputDataPtr = static_cast<uint8_t*>(dataPtr);
             inputLen = len;
-
-            VLOG(L1, "Setting input data for Dequantize OP");
             return true;
         }
 
         std::tuple<void*, int32_t> getOutputData() {
-            VLOG(L1, "%s output len:%d", __func__, inputLen);
             return std::make_tuple((void*)output, inputLen);
         }
 
@@ -41,33 +38,21 @@ class DequantizeOp : public BaseOp {
             inputDataPtr = nullptr;
             inputLen = 0;
             layerName = name;
-
-            VLOG(L1, "%s scalefactor:%f zeropoint:%d", __func__, sc, zp);
         }
 
         bool  run() {
             const T* inputBuf = reinterpret_cast<const T*>(inputDataPtr);
-
-            VLOG(L1, "%s inputLen:%d zeroPoint=%d scale:%f", __func__, inputLen, zeroPoint, scale);
             output = new float[inputLen];
-
-            for(auto i =0; i < 4; i++)
-                VLOG(L1, "Input at index:%d is :%d", i, inputBuf[i]);
 
             for (int i = 0; i < inputLen; ++i) {
                 int32_t value = inputBuf[i];
                 output[i] = static_cast<float>(scale * (value - zeroPoint));
-
-                if (i < 4) {
-                    VLOG(L1, "Index:%d input:%d Output:%f", i, value, output[i]);
-                }
-            }    
+            }
 
             return true;
         }
 
         float*  run(const uint8_t* inputData, const uint32_t len) {
-            VLOG(L1, "%s ", __func__);
             output = new float[len];
 
             int32_t value;
@@ -81,6 +66,11 @@ class DequantizeOp : public BaseOp {
         }
 
         std::string getLayerName() { return layerName; }
+
+		void cleanup() {
+			if (output)
+				delete output;
+		}
 
         ~DequantizeOp() {
             if (output) {
