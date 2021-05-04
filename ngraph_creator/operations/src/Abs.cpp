141b2f76057a3852c741dab3003d831bc1bb30a1
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-04 20:19:31 +0530

    Adding ABS operation implementation
    Implemented ABS operation
    
    Signed-off-by: Nagamani Chennuboina nagamani.chennuboina@intel.com

---
+++ ngraph_creator/operations/src/Abs.cpp
@@ -0,0 +1,42 @@
+#include <Abs.hpp>
+
+namespace android {
+namespace hardware {
+namespace neuralnetworks {
+namespace nnhal {
+
+Abs::Abs(int operationIndex) : OperationsBase(operationIndex) {
+    mDefaultOutputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+}
+
+bool Abs::validate() {
+    // check output type
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        return false;
+    }
+
+    // Check all input types
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        return false;
+    }
+
+    return true;
+}
+
+std::shared_ptr<ngraph::Node> Abs::createNode() {
+    // Creating input nodes
+    auto input = getInputNode<float>(0);
+
+    auto outputNode = std::make_shared<ngraph::opset3::Abs>(input);
+
+    const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
+    if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
+        addResultNode(mDefaultOutputIndex, outputNode);
+    }
+    return outputNode;
+}
+
+}  // namespace nnhal
+}  // namespace neuralnetworks
+}  // namespace hardware
+}  // namespace android
