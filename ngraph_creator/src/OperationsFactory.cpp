3218a0625650f98678d847e797d48a8f15c82bf4
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-04 23:14:43 +0530

    Adding Reduce_Min operation implementation
    Implemented Reduce_Min operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/src/OperationsFactory.cpp
+++ ngraph_creator/src/OperationsFactory.cpp
@@ -79,6 +79,8 @@ std::shared_ptr<OperationsBase> OperationsFactory::getOperation(
             return std::make_shared<Reduce_All>(operationIndex);
         case OperationType::REDUCE_ANY:
             return std::make_shared<Reduce_Any>(operationIndex);
+        case OperationType::REDUCE_MIN:
+            return std::make_shared<Reduce_Min>(operationIndex);
         case OperationType::RESHAPE:
             return std::make_shared<Reshape>(operationIndex);
         case OperationType::SOFTMAX:
