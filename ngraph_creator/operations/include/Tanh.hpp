bc11f468ef758d295b0c70a2819498e989d9cdc9
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-05 00:09:04 +0530

    Adding Tanh operation implementation
    Implemented Tanh operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

---
+++ ngraph_creator/operations/include/Tanh.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <OperationsBase.hpp>
+
+namespace android {
+namespace hardware {
+namespace neuralnetworks {
+namespace nnhal {
+
+class Tanh : public OperationsBase {
+public:
+    Tanh(int operationIndex);
+    bool validate() override;
+    std::shared_ptr<ngraph::Node> createNode() override;
+};
+
+}  // namespace nnhal
+}  // namespace neuralnetworks
+}  // namespace hardware
+}  // namespace android
---
+++ ngraph_creator/operations/src/Tanh.cpp
@@ -0,0 +1,42 @@
+#include <Tanh.hpp>
+
+namespace android {
+namespace hardware {
+namespace neuralnetworks {
+namespace nnhal {
+
+Tanh::Tanh(int operationIndex) : OperationsBase(operationIndex) {
+    mDefaultOutputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+}
+
+bool Tanh::validate() {
+    // check output type
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        return false;
+    }
+
+    // Check all input types
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        return false;
+    }
+
+    return true;
+}
+
+std::shared_ptr<ngraph::Node> Tanh::createNode() {
+    // Creating input nodes
+    auto input = getInputNode<float>(0);
+
+    auto outputNode = std::make_shared<ngraph::opset3::Tanh>(input);
+
+    const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
+    if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
+        addResultNode(mDefaultOutputIndex, outputNode);
+    }
+    return outputNode;
+}
+
+}  // namespace nnhal
+}  // namespace neuralnetworks
+}  // namespace hardware
+}  // namespace android
