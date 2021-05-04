eddeee13e08b39380ee14a265f116c84207e2025
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-04 20:10:48 +0530

    Adding Add operation implementation
    Implemented Add operation
    
    Signed-off-by: Nagamani Chennuboina nagamani.chennuboina@intel.com

--- ngraph_creator/operations/src/Add.cpp
+++ ngraph_creator/operations/src/Add.cpp
@@ -5,19 +5,45 @@ namespace hardware {
 namespace neuralnetworks {
 namespace nnhal {
 
-Add::Add(int operationIndex) : OperationsBase(operationIndex) {}
+Add::Add(int operationIndex) : OperationsBase(operationIndex) {
+    mDefaultOutputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+}
 
-bool Add::validate() { return true; }
+bool Add::validate() {
+    // check output type
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        return false;
+    }
 
-// TODO: Implement APIs createNode & createNodeForPlugin. These are just dummy placeholders.
+    // Check all input types
+    for (int i = 0; i <= 1; i++) {
+        if (!checkInputOperandType(i, (int32_t)OperandType::TENSOR_FLOAT32)) return false;
+    }
+
+    if (!checkInputOperandType(2, (int32_t)OperandType::INT32)) {
+        return false;
+    }
+
+    return true;
+}
+
 std::shared_ptr<ngraph::Node> Add::createNode() {
-    auto input = mNgraphNodes->getOperationOutput(
-        sModelInfo->getOperationInput(mNnapiOperationIndex, OP_INPUT_IDX_CONV));
-    std::shared_ptr<ngraph::Node> constantOp =
-        std::make_shared<ngraph::opset3::Constant>(ngraph::element::f32, input.get_shape());
-    auto transposedOp = transpose(NHWC_NCHW, constantOp);
-    return std::make_shared<ngraph::opset3::Add>(input, transposedOp,
-                                                 ngraph::op::AutoBroadcastType::NUMPY);
+    // Creating input nodes
+    auto input1 = getInputNode<float>(0);
+    auto input2 = getInputNode<float>(1);
+
+    auto activationFn = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 2);
+
+    auto addNode =
+        std::make_shared<ngraph::opset3::Add>(input1, input2, ngraph::op::AutoBroadcastType::NUMPY);
+
+    auto outputNode = applyActivation(addNode, activationFn);
+
+    const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
+    if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
+        addResultNode(mDefaultOutputIndex, outputNode);
+    }
+    return outputNode;
 }
 
 std::shared_ptr<ngraph::Node> Add::createNodeForPlugin() {
