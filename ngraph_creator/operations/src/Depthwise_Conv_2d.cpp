1921de9f86820c14e9df660456a532c34eaeec21
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-19 19:35:13 +0530

    Fixed VTS issues for Depthwise_Conv_2d operation
    1. Added support for 1.0, 1.1 inputs
    2. Fixed "NCHW" output conversion
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/operations/src/Depthwise_Conv_2d.cpp
+++ ngraph_creator/operations/src/Depthwise_Conv_2d.cpp
@@ -30,30 +30,50 @@ bool Depthwise_Conv_2d::validate() {
     }
 
     const auto& filterDimensions = getInputOperandDimensions(1);
-    if (filterDimensions[0] != 1)
+    if (filterDimensions[0] != 1) {
         ALOGE("%s Invalid dimension at filter[0] (%d)", __func__, filterDimensions[0]);
-    ;
+        return false;
+    }
 
     const auto& inputsSize = sModelInfo->getOperationInputsSize(mNnapiOperationIndex);
-    if (inputsSize == 14) {
-        // Check all other Input operand types for explicit Padding
-        for (int i = 3; i < 14; i++) {
-            // All inputs except index 11 should be INT32
-            // index 11 should be BOOL
-            if (i == 11) {
-                if (!checkInputOperandType(i, (int32_t)OperandType::BOOL)) return false;
-            } else if (!checkInputOperandType(i, (int32_t)OperandType::INT32))
-                return false;
-        }
-    } else if (inputsSize == 11) {
-        // Check all other Input operand types for implicit Padding
+
+    if (inputsSize >= 11 && inputsSize <= 14 &&
+        !checkInputOperandType(8, (int32_t)OperandType::BOOL)) {
+        // Checking input types for explicit padding
         for (int i = 3; i < 11; i++) {
-            // All inputs except index 8 should be INT32
-            // index 8 should be BOOL
-            if (i == 8) {
-                if (!checkInputOperandType(i, (int32_t)OperandType::BOOL)) return false;
-            } else if (!checkInputOperandType(i, (int32_t)OperandType::INT32))
-                return false;
+            if (!checkInputOperandType(i, (int32_t)OperandType::INT32)) return false;
+        }
+
+        if (inputsSize > 11 && inputsSize <= 14) {
+            switch (inputsSize) {
+                case 14:
+                    if (!checkInputOperandType(13, (int32_t)OperandType::INT32)) return false;
+                    ;
+                case 13:
+                    if (!checkInputOperandType(12, (int32_t)OperandType::INT32)) return false;
+                case 12:
+                    if (!checkInputOperandType(11, (int32_t)OperandType::BOOL)) return false;
+                default:
+                    break;
+            }
+        }
+    } else if (inputsSize >= 8 && inputsSize <= 11) {
+        // Checking input types for implicit padding
+        for (int i = 3; i < 8; i++) {
+            if (!checkInputOperandType(i, (int32_t)OperandType::INT32)) return false;
+        }
+
+        if (inputsSize > 8 && inputsSize <= 11) {
+            switch (inputsSize) {
+                case 11:
+                    if (!checkInputOperandType(10, (int32_t)OperandType::INT32)) return false;
+                case 10:
+                    if (!checkInputOperandType(9, (int32_t)OperandType::INT32)) return false;
+                case 9:
+                    if (!checkInputOperandType(8, (int32_t)OperandType::BOOL)) return false;
+                default:
+                    break;
+            }
         }
     }
 
@@ -64,6 +84,14 @@ bool Depthwise_Conv_2d::validate() {
 std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
     const auto& inputsSize = sModelInfo->getOperationInputsSize(mNnapiOperationIndex);
     ALOGD("%s inputsSize %d", __func__, inputsSize);
+    bool isImplicit = false, isExplicit = false;
+
+    if (inputsSize >= 11 && inputsSize <= 14 &&
+        !checkInputOperandType(8, (int32_t)OperandType::BOOL)) {
+        isExplicit = true;
+    } else if (inputsSize >= 8 && inputsSize <= 11) {
+        isImplicit = true;
+    }
 
     int32_t padding_left, padding_right;
     int32_t padding_top, padding_bottom;
@@ -82,21 +110,15 @@ std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
     std::vector<size_t> dilations;
     ngraph::op::PadType auto_pad;
 
-    {
-        const auto& inputDimensions = getInputOperandDimensions(0);
-        input_width = inputDimensions[2];
-        input_height = inputDimensions[1];
-        input_channel = inputDimensions[3];
-    }
+    const auto& inputDimensions = getInputOperandDimensions(0);
+
     {
         const auto& filterDimensions = getInputOperandDimensions(1);
         filter_width = filterDimensions[2];
         filter_height = filterDimensions[1];
     }
 
-    if (inputsSize == 14) {
-        // Explicit padding
-
+    if (isExplicit) {
         padding_left = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 3);
         padding_right = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 4);
         padding_top = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 5);
@@ -107,19 +129,40 @@ std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
 
         depthwise_multiplier = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 9);
 
-        dilation_width_factor = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 12);
-        dilation_height_factor =
-            sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 13);
-
         activationFn = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 10);
-        layout = sModelInfo->ParseOperationInput<uint8_t>(mNnapiOperationIndex, 11);
+
+        if (inputsSize > 11 && inputsSize <= 14) {
+            switch (inputsSize) {
+                case 14:
+                    dilation_height_factor =
+                        sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 13);
+                case 13:
+                    dilation_width_factor =
+                        sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 12);
+                case 12:
+                    layout = sModelInfo->ParseOperationInput<uint8_t>(mNnapiOperationIndex, 11);
+                default:
+                    break;
+            }
+        }
 
         if (layout) useNchw = true;
 
         auto_pad = ngraph::op::PadType::EXPLICIT;
+        {
+            if (useNchw) {
+                input_width = inputDimensions[3];
+                input_height = inputDimensions[2];
+                input_channel = inputDimensions[1];
+            } else {
+                input_width = inputDimensions[2];
+                input_height = inputDimensions[1];
+                input_channel = inputDimensions[3];
+            }
+        }
+    }
 
-    } else if (inputsSize == 11) {
-        // Implicit padding
+    if (isImplicit) {
         padding_scheme = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 3);
 
         stride_width = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 4);
@@ -127,43 +170,36 @@ std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
 
         depthwise_multiplier = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 6);
 
-        dilation_width_factor = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 9);
-        dilation_height_factor =
-            sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 10);
-
         activationFn = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 7);
-        layout = sModelInfo->ParseOperationInput<uint8_t>(mNnapiOperationIndex, 8);
+
+        if (inputsSize > 8 && inputsSize <= 11) {
+            switch (inputsSize) {
+                case 11:
+                    dilation_height_factor =
+                        sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 10);
+                case 10:
+                    dilation_width_factor =
+                        sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 9);
+                case 9:
+                    layout = sModelInfo->ParseOperationInput<uint8_t>(mNnapiOperationIndex, 8);
+                default:
+                    break;
+            }
+        }
 
         if (layout) useNchw = true;
 
-        if (padding_scheme == 1) {
-            calculateExplicitPadding(input_width, stride_width, filter_width, 1, &padding_left,
-                                     &padding_right);
-            calculateExplicitPadding(input_height, stride_height, filter_height, 1, &padding_top,
-                                     &padding_bottom);
-            auto_pad = ngraph::op::PadType::SAME_UPPER;
-        } else if (padding_scheme == 2) {
-            auto_pad = ngraph::op::PadType::VALID;
-            padding_left = 0;
-            padding_right = 0;
-            padding_top = 0;
-            padding_bottom = 0;
-        } else {
-            auto_pad = ngraph::op::PadType::NOTSET;
+        {
+            if (useNchw) {
+                input_width = inputDimensions[3];
+                input_height = inputDimensions[2];
+                input_channel = inputDimensions[1];
+            } else {
+                input_width = inputDimensions[2];
+                input_height = inputDimensions[1];
+                input_channel = inputDimensions[3];
+            }
         }
-    } else if (inputsSize == 8) {
-        // implicit padding, 1.0
-        padding_scheme = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 3);
-
-        stride_width = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 4);
-        stride_height = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 5);
-
-        depthwise_multiplier = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 6);
-
-        dilation_width_factor = 1;
-        dilation_height_factor = 1;
-
-        activationFn = sModelInfo->ParseOperationInput<uint32_t>(mNnapiOperationIndex, 7);
 
         if (padding_scheme == 1) {
             calculateExplicitPadding(input_width, stride_width, filter_width, 1, &padding_left,
@@ -208,17 +244,17 @@ std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
     pads_end = {padding_right, padding_bottom};
     dilations = {(size_t)dilation_width_factor, (size_t)dilation_height_factor};
 
-    if (input_channel != 1) {
-        if (filterNode != nullptr) {
-            std::vector<size_t> shape(&filterNode->get_shape()[0], &filterNode->get_shape()[0] + 4);
-            shape[0] /= input_channel;
-            shape.insert(shape.begin(), input_channel);
-            ALOGD("%s final filternode shape %d", __func__, shape.size());
+    ALOGD("input_channel value is ******* %d", input_channel);
 
-            auto shapeNode = std::make_shared<ngraph::op::Constant>(
-                ngraph::element::i64, ngraph::Shape{shape.size()}, shape.data());
-            filterNode = std::make_shared<ngraph::op::v1::Reshape>(filterNode, shapeNode, true);
-        }
+    if (filterNode != nullptr) {
+        std::vector<size_t> shape(&filterNode->get_shape()[0], &filterNode->get_shape()[0] + 4);
+        shape[0] /= input_channel;
+        shape.insert(shape.begin(), input_channel);
+        ALOGD("%s final filternode shape %d", __func__, shape.size());
+
+        auto shapeNode = std::make_shared<ngraph::op::Constant>(
+            ngraph::element::i64, ngraph::Shape{shape.size()}, shape.data());
+        filterNode = std::make_shared<ngraph::op::v1::Reshape>(filterNode, shapeNode, true);
     }
 
     auto groupConvNode = std::make_shared<ngraph::opset3::GroupConvolution>(
@@ -239,8 +275,10 @@ std::shared_ptr<ngraph::Node> Depthwise_Conv_2d::createNode() {
 
     const auto outputLifetime = sModelInfo->getOperandLifetime(mDefaultOutputIndex);
     if (outputLifetime == OperandLifeTime::MODEL_OUTPUT) {
-        outputNode = transpose(NCHW_NHWC, outputNode);
-        mNgraphNodes->setForcedNchw(mDefaultOutputIndex, false);
+        if (!useNchw) {
+            outputNode = transpose(NCHW_NHWC, outputNode);
+            mNgraphNodes->setForcedNchw(mDefaultOutputIndex, false);
+        }
         addResultNode(mDefaultOutputIndex, outputNode);
     }
     return outputNode;
