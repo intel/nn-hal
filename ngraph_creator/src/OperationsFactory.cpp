0aa98df7526bf1155a811c0d7a1e3de3b6833f0a
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-31 13:51:04 +0530

    Adding Log_Softmax operation implementation
    - Implemented Log_Softmax operation
    - added Quant support
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/src/OperationsFactory.cpp
+++ ngraph_creator/src/OperationsFactory.cpp
@@ -53,6 +53,8 @@ std::shared_ptr<OperationsBase> OperationsFactory::getOperation(
             return std::make_shared<Less>(operationIndex);
         case OperationType::LESS_EQUAL:
             return std::make_shared<Less_Equal>(operationIndex);
+        case OperationType::LOG_SOFTMAX:
+            return std::make_shared<Log_Softmax>(operationIndex);
         case OperationType::LOG:
             return std::make_shared<Log>(operationIndex);
         case OperationType::LOGICAL_AND:
