5a91202d0a583257d61d509f9bc309ed3ba6142d
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-31 12:01:08 +0530

    Adding Quant support to Cast operation
    Added TENSOR_QUANT8_ASYMM tensor support to Cast operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/operations/src/Cast.cpp
+++ ngraph_creator/operations/src/Cast.cpp
@@ -12,13 +12,15 @@ Cast::Cast(int operationIndex) : OperationsBase(operationIndex) {
 bool Cast::validate() {
     // check output type
     if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
-        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_INT32) &&
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
 
     // Check all input types
     if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
-        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_INT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
 
@@ -31,20 +33,26 @@ std::shared_ptr<ngraph::Node> Cast::createNode() {
 
     if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
         input = getInputNode<float>(0);
-    }
-
-    if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
         input = getInputNode<int>(0);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        input = getInputNode<uint8_t>(0);
     }
 
     ngraph::element::Type elementType;  // change to outputbased element type
 
     if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
         elementType = ngraph::element::f32;
-    }
-
-    if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
+    } else if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_INT32)) {
         elementType = ngraph::element::i32;
+    } else if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+        auto minVal = ngraph::op::Constant::create(ngraph::element::i32, ngraph::Shape{}, {0});
+        auto maxVal = ngraph::op::Constant::create(ngraph::element::i32, ngraph::Shape{}, {255});
+        auto convertInput = std::make_shared<ngraph::opset3::Convert>(input, ngraph::element::i32);
+        auto min = std::make_shared<ngraph::opset3::Minimum>(maxVal, convertInput);
+        input = std::make_shared<ngraph::opset3::Maximum>(minVal, min);
+        elementType = ngraph::element::u8;
     }
 
     auto outputNode = std::make_shared<ngraph::opset3::Convert>(input, elementType);
