c710cfad83f038e620e4926cbe03cccf62fd11e4
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-31 13:03:36 +0530

    Adding Quant support to Tanh operation
    Added TENSOR_QUANT8_ASYMM tensor support to Tanh operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/operations/src/Tanh.cpp
+++ ngraph_creator/operations/src/Tanh.cpp
@@ -11,12 +11,14 @@ Tanh::Tanh(int operationIndex) : OperationsBase(operationIndex) {
 
 bool Tanh::validate() {
     // check output type
-    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
 
     // Check all input types
-    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
 
@@ -25,9 +27,23 @@ bool Tanh::validate() {
 
 std::shared_ptr<ngraph::Node> Tanh::createNode() {
     // Creating input nodes
-    auto input = getInputNode<float>(0);
+    std::shared_ptr<ngraph::Node> input;
 
-    auto outputNode = std::make_shared<ngraph::opset3::Tanh>(input);
+    if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        input = getInputNode<float>(0);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        input = getInputNode<uint8_t>(0);
+        const auto& inputIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 0);
+        input = DequantizeNode(input, inputIndex, ngraph::element::f32);
+    }
+    std::shared_ptr<ngraph::Node> outputNode;
+
+    outputNode = std::make_shared<ngraph::opset3::Tanh>(input);
+
+    if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+        outputNode = QuantizeNode(outputNode, outputIndex, ngraph::element::u8);
+    }
 
     const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
     if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
