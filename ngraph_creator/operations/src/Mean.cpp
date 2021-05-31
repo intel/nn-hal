d60230312f2027ab9e0701833341f2abaca8eb60
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-31 12:32:38 +0530

    Adding Quant support to Mean operation
    Added TENSOR_QUANT8_ASYMM tensor support to Mean operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/operations/src/Mean.cpp
+++ ngraph_creator/operations/src/Mean.cpp
@@ -11,12 +11,15 @@ Mean::Mean(int operationIndex) : OperationsBase(operationIndex) {
 
 bool Mean::validate() {
     // check output type
-    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
 
     // Check all input types
-    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) return false;
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
+        return false;
 
     if (!checkInputOperandType(1, (int32_t)OperandType::TENSOR_INT32)) return false;
 
@@ -35,13 +38,26 @@ bool Mean::validate() {
 
 std::shared_ptr<ngraph::Node> Mean::createNode() {
     // Creating input nodes
-    auto input = getInputNode<float>(0);
+    std::shared_ptr<ngraph::Node> input;
+
+    if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        input = getInputNode<float>(0);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        input = getInputNode<uint8_t>(0);
+        const auto& inputIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 0);
+        input = DequantizeNode(input, inputIndex, ngraph::element::f32);
+    }
     auto reduction_axes = getInputNode<int>(1);
     auto reduce_dims = sModelInfo->ParseOperationInput<int>(mNnapiOperationIndex, 2);
     bool keep_dims = (reduce_dims > 0) ? true : false;
 
-    auto outputNode =
-        std::make_shared<ngraph::opset3::ReduceMean>(input, reduction_axes, keep_dims);
+    std::shared_ptr<ngraph::Node> outputNode;
+    outputNode = std::make_shared<ngraph::opset3::ReduceMean>(input, reduction_axes, keep_dims);
+
+    if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+        outputNode = QuantizeNode(outputNode, outputIndex, ngraph::element::u8);
+    }
 
     const auto op = sModelInfo->getOperand(mDefaultOutputIndex);
     if (op.lifetime == OperandLifeTime::MODEL_OUTPUT) {
