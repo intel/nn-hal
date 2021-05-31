d796437525d42c852fa0e58f7443690035a34069
Nagamani Chennuboina nagamani.chennuboina@intel.com <nagamani.chennuboina@intel.com>
2021-05-31 13:07:08 +0530

    Implemented Softmax completely and added Quant support
    - Implemented Softmax
    - Fixed VTS/CTS softmax issues
    - Added TENSOR_QUANT8_ASYMM tensor support to Softmax operation
    
    Signed-off-by: Nagamani Chennuboina <nagamani.chennuboina@intel.com>

--- ngraph_creator/operations/src/Softmax.cpp
+++ ngraph_creator/operations/src/Softmax.cpp
@@ -12,29 +12,67 @@ Softmax::Softmax(int operationIndex) : OperationsBase(operationIndex) {
 }
 
 bool Softmax::validate() {
-    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
-    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
         return false;
     }
     if (!checkInputOperandType(1, (int32_t)OperandType::FLOAT32)) {
         return false;
+    }
+    const auto& inputsSize = sModelInfo->getOperationInputsSize(mNnapiOperationIndex);
+    if (inputsSize == 3) {
+        if (!checkInputOperandType(2, (int32_t)OperandType::INT32)) {
+            return false;
+        }
     }
     ALOGV("%s PASSED", __func__);
     return true;
 }
 
 std::shared_ptr<ngraph::Node> Softmax::createNode() {
-    auto inputIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 0);
-    auto inputOp = mNgraphNodes->getOperationOutput(inputIndex);
+    // Creating input nodes
+    std::shared_ptr<ngraph::Node> input;
 
-    std::shared_ptr<ngraph::Node> outputNode = std::make_shared<ngraph::opset3::Softmax>(inputOp);
+    if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) {
+        input = getInputNode<float>(0);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        input = getInputNode<uint8_t>(0);
+        const auto& inputIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 0);
+        input = DequantizeNode(input, inputIndex, ngraph::element::f32);
+    }
 
     float beta = sModelInfo->ParseOperationInput<float>(mNnapiOperationIndex, 1);
-    ALOGV("Softmax beta = %f ", beta);
+    auto betaNode = ngraph::opset3::Constant::create(ngraph::element::f32, {}, {beta});
+    int axis = -1;
+    const auto& inputsSize = sModelInfo->getOperationInputsSize(mNnapiOperationIndex);
+    if (inputsSize == 3) axis = sModelInfo->ParseOperationInput<int>(mNnapiOperationIndex, 2);
 
-    if (beta <= 0.0f) ALOGE("beta must be positive for softmax");
+    const auto axisNode =
+        ngraph::opset3::Constant::create(ngraph::element::i64, ngraph::Shape{1}, {axis});
+    // max(input[batch, :]
+    auto max = std::make_shared<ngraph::opset3::ReduceMax>(input, axisNode, true);
+    // input[batch, i] - max(input[batch, :])
+    auto sub = std::make_shared<ngraph::opset3::Subtract>(input, max);
+    // (input[batch, i] - max(input[batch, :])) * beta
+    auto mul = std::make_shared<ngraph::opset3::Multiply>(sub, betaNode);
+    // exp((input[batch, i] - max(input[batch, :])) * beta)
+    auto exp = std::make_shared<ngraph::opset3::Exp>(mul);
+    // sum_{k}{exp((input[batch, k] - max(input[batch, :])) * beta)}
+    auto sum = std::make_shared<ngraph::opset3::ReduceSum>(exp, axisNode, true);
+
+    std::shared_ptr<ngraph::Node> outputNode;
+    // exp((input[batch, i] - max(input[batch, :])) * beta) / sum_{k}{exp((input[batch, k] -
+    // max(input[batch, :])) * beta)}
+    outputNode = std::make_shared<ngraph::opset3::Divide>(exp, sum);
+
+    if (checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        const auto& outputIndex = sModelInfo->getOperationOutput(mNnapiOperationIndex, 0);
+        outputNode = QuantizeNode(outputNode, outputIndex, ngraph::element::u8);
+    }
 
     const auto outputOperand = sModelInfo->getOperand(mDefaultOutputIndex);
     if (outputOperand.lifetime == OperandLifeTime::MODEL_OUTPUT)
