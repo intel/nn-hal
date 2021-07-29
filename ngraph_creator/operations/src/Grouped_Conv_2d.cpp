d4789845b8c5adbadeb9dc067db14f642981bb93
Cooper Chiou <cooper.chiou@intel.com>
2021-07-30 00:21:51 +0800

    Add TENSOR_QUANT8_SYMM_PER_CHANNEL support for Grouped_Conv_2d
    Signed-off-by: Cooper Chiou <cooper.chiou@intel.com>

--- ngraph_creator/operations/src/Grouped_Conv_2d.cpp
+++ ngraph_creator/operations/src/Grouped_Conv_2d.cpp
@@ -13,21 +13,38 @@ Grouped_Conv_2d::Grouped_Conv_2d(int operationIndex) : OperationsBase(operationI
 
 bool Grouped_Conv_2d::validate() {
     // Check Output type
-    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32)) return false;
+    if (!checkOutputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkOutputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
+        return false;
 
-    for (int i = 0; i <= 2; i++) {
-        // Check input/filter/bias operands(0/1/2) are of type TENSOR_FLOAT32
-        if (!checkInputOperandType(i, (int32_t)OperandType::TENSOR_FLOAT32)) return false;
-    }
+    // Check input/filter operands(0/1) are of type TENSOR_FLOAT32/TENSOR_QUANT8_ASYMM
+    if (!checkInputOperandType(0, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM))
+        return false;
+    if (!checkInputOperandType(1, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_ASYMM) &&
+        !checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL))
+        return false;
+    // Check bias type
+    if (!checkInputOperandType(2, (int32_t)OperandType::TENSOR_FLOAT32) &&
+        !checkInputOperandType(2, (int32_t)OperandType::TENSOR_INT32))
+        return false;
 
     // Check Input, Filter Dimension size
     const auto& inputDimensionsSize = getInputOperandDimensions(0).size();
     const auto& filterDimensionsSize = getInputOperandDimensions(1).size();
-
     if (inputDimensionsSize != 4 || filterDimensionsSize != 4) {
         ALOGE("%s Invalid dimensions size for input(%lu) or filter(%lu)", __func__,
               inputDimensionsSize, filterDimensionsSize);
         return false;
+    }
+
+    if (checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL)) {
+        const auto& operandIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 1);
+        const auto& operand = sModelInfo->getOperand(operandIndex);
+        if (operand.extraParams.channelQuant().channelDim != 0) {
+            return false;
+        }
     }
 
     ALOGV("%s PASSED", __func__);
@@ -144,8 +161,43 @@ std::shared_ptr<ngraph::Node> Grouped_Conv_2d::createNode() {
         }
     }
 
-    auto inputNode = getInputNode(0);
-    auto filterNode = getInputNode(1);
+    std::shared_ptr<ngraph::Node> inputNode, filterNode, biasNode;
+
+    const auto& inputIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 0);
+    const auto& filterIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 1);
+    const auto& biasIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 2);
+
+    inputNode = getInputNode(0);
+    filterNode = getInputNode(1);
+    biasNode = getInputNode(2);
+
+    if (checkInputOperandType(1, (int32_t)OperandType::TENSOR_QUANT8_SYMM_PER_CHANNEL)) {
+        auto filterIndex = sModelInfo->getOperationInput(mNnapiOperationIndex, 1);
+        const auto& filterOperand = sModelInfo->getOperand(filterIndex);
+        vec<float> filterScales = filterOperand.extraParams.channelQuant().scales;
+        float inputScale = sModelInfo->getOperandScale(0);
+        auto filterScales_node =
+            createConstNode(ngraph::element::f32, ngraph::Shape{filterScales.size()}, filterScales);
+        auto inputScales_node =
+            createConstNode(ngraph::element::f32, ngraph::Shape{1}, convertToVector(inputScale));
+        filterNode = transpose(NHWC_CWHN, filterNode);
+        auto filterNodeConvert =
+            std::make_shared<ngraph::opset3::Convert>(filterNode, ngraph::element::f32);
+        auto filterNodeMulScales =
+            std::make_shared<ngraph::opset3::Multiply>(filterNodeConvert, filterScales_node);
+        filterNode = transpose(CWHN_NHWC, filterNodeMulScales);
+        // for quant symm per channel type inputs, bias is of type TENSOR_INT32. For TENSOR_INT32
+        // type, dequantization is not applied during node creation
+        // bias_scale[i] = input_scale * filter_scale[i]
+        auto biasScalMultiplier =
+            std::make_shared<ngraph::opset3::Multiply>(filterScales_node, inputScales_node);
+        biasNode = std::make_shared<ngraph::opset3::Convert>(biasNode, ngraph::element::f32);
+        biasNode = std::make_shared<ngraph::opset3::Multiply>(biasNode, biasScalMultiplier);
+    } else if (checkInputOperandType(0, (int32_t)OperandType::TENSOR_QUANT8_ASYMM)) {
+        // for quant type inputs, bias is of type TENSOR_INT32. For TENSOR_INT32 type,
+        // dequantization is not applied during node creation
+        biasNode = DequantizeNode(biasNode, biasIndex, ngraph::element::f32);
+    }
 
     // OpenVino expects filter in OIHW format
     filterNode = transpose(OHWI_OIHW, filterNode);
@@ -179,7 +231,6 @@ std::shared_ptr<ngraph::Node> Grouped_Conv_2d::createNode() {
         inputNode, filterNode, ngraph::Strides(strides), ngraph::CoordinateDiff(pads_begin),
         ngraph::CoordinateDiff(pads_end), ngraph::Strides(dilations), auto_pad);
 
-    auto biasNode = getInputNode(2);
     auto biasDimensions = getInputOperandDimensions(2);
     std::vector<uint32_t> shape(groupConvNode->get_shape().size(), 1);
     shape[1] = biasDimensions[0];
