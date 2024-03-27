9d73779c6f1335453437e1b89628a900fd80f800
Ritul Jasuja <ritul.jasuja@intel.com>
2024-03-27 12:51:01 +0530

    Remove enable-sep-transposed-conv from compilation params
    Removes enable-sep-transposed-conv from compilation params as
    it's not supported
    
    Signed-off-by: Ritul Jasuja <ritul.jasuja@intel.com>

--- IENetwork.cpp
+++ IENetwork.cpp
@@ -39,7 +39,7 @@ bool IENetwork::loadNetwork() {
     ALOGD("Creating infer request for Intel Device Type : %s", deviceStr.c_str());
     if (mNetwork) {
 	ov::AnyMap config;
-	config["NPU_COMPILATION_MODE_PARAMS"] = "enable-se-ptrs-operations=true enable-sep-transposed-conv=true";
+	config["NPU_COMPILATION_MODE_PARAMS"] = "enable-se-ptrs-operations=true";
 	compiled_model = ie.compile_model(mNetwork, deviceStr, config);
         ALOGD("LoadNetwork is done....");
 
